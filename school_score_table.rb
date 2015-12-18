11574f1254eef44858ce4c0b5f681ff8dd97ec2f
Dan Andersen & Pete <pair+dandersen2.roytuesday@devbootcamp.com>
2015-12-18 11:19:17 -0500

    Finish SchoolScoreTable class

--- school_score_table.rb
+++ school_score_table.rb
@@ -1,2 +1,14 @@
 class SchoolScoreTable
-  def
+  def initialize(scores)
+    @school_scores = populate_table(scores)
+  end
+
+  def populate_table(scores)
+    scores.map do |score|
+      SchoolScore.new
+    end
+  end
+
+end
+
+
