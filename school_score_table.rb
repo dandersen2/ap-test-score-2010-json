428976d64326dc77753ec4a038cffe3ffa35fbff
Dan Andersen & Pete <pair+dandersen2.roytuesday@devbootcamp.com>
2015-12-18 11:23:02 -0500

    Add attr_reader for SSTable class

--- school_score_table.rb
+++ school_score_table.rb
@@ -1,12 +1,12 @@
 class SchoolScoreTable
+  attr_reader :school_scores
+
   def initialize(scores)
     @school_scores = populate_table(scores)
   end
 
   def populate_table(scores)
-    scores.map do |score|
-      SchoolScore.new
-    end
+    scores.map { |score| SchoolScore.new(score) }
   end
 
 end
